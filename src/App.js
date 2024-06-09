fb39842096f32e41a69370115e78e9a5cb65b84a
martine9696 <gogulnithin07>
2024-06-09 20:43:12 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Login/>
     </div>
   );
 }
 
+function Login(){
+  const [error,setError]=useState("");
+  const obj={
+    userName:"gogul",
+    password:"123",
+  }
+
+function handleSubmit(e){
+  e.preventDefault();
+const username=e.target.Username;
+const password=e.target.Password;
+if(username.value===obj.userName && password.value===obj.password){
+  username.value=""
+  password.value=""
+setError(false);
+}else{
+  setError(true);
+  username.value=""
+  password.value=""
+}
+}
+  return <div>
+    <h1>Login Page</h1>
+    {error===true&& <h2>Invalid username or password</h2>}
+    {error===false&& <h2>Welcome, user!</h2>}
+   {error===true ||error==="" ? <form onSubmit={(e)=>handleSubmit(e)}>
+   <div>
+     <label htmlFor='Username'>Username:</label>
+     <input placeholder='username' type='text' id='Username' required/>
+   </div>
+   <div>
+     <label htmlFor='Password'>Password:</label>
+     <input placeholder='password' type="password" id='Password' required/>
+   </div>
+   <button type='submit'>Submit</button>
+ </form>:null
+  }
+  </div>
+}
 export default App;
